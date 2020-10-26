421048a2a5e5ba599ef81e443777af76e5d429cc
ParkhomenkoAlexey <Parkhomenko_ad@mail.ru>
2020-10-26 14:50:10 +0500

    install oneSignalPod and add notificationService

--- OneSignalTestApp/ViewController.swift
+++ OneSignalTestApp/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myTextView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonTapped(_ sender: Any) {
+        print("1")
+    }
+    
 }
 
---
+++ Pods/Target Support Files/Pods-OneSignalTestApp/Pods-OneSignalTestApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OneSignalTestApp : NSObject
+@end
+@implementation PodsDummy_Pods_OneSignalTestApp
+@end
